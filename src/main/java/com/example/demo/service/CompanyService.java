a384c973c1fa9f46f648263a35c8ef5c7df97ec4
DESKTOP-8N3GG4O\GGG <klk510a@gmail.com>
2024-12-02 17:20:28 +0900

    12.02/17:20 박준택 커밋

---
+++ src/main/java/com/example/demo/service/CompanyService.java
@@ -0,0 +1,63 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.ICompanyDao;
+import com.example.demo.dto.CompanyDto;
+import com.example.demo.dto.RatingDto;
+
+@Service
+public class CompanyService implements ICompanyService{
+	
+	@Autowired
+	ICompanyDao companyDao;
+
+
+
+	@Override
+	public List<CompanyDto> getCompanyList() {
+		
+		List<CompanyDto> companys = companyDao.getCompanyList();
+		return companys;
+	}
+
+
+
+	@Override
+	public List<CompanyDto> getCompanyListPaging(int startNum, int amount) {
+		
+		List<CompanyDto> companys = companyDao.getCompanyListPaging(startNum, amount);
+		return companys;
+	}
+
+
+
+	@Override // 총 페이지 갯수 구하려고 만듬
+	public int getCount() {
+		
+		int pageCnt = companyDao.getCount();
+		return pageCnt;
+	}
+
+
+
+	@Override
+	public CompanyDto getCompany(String companyName) {
+		CompanyDto company=companyDao.getCompany(companyName);
+		return company;
+	}
+
+
+
+	@Override
+	public RatingDto companyRating(String companyId) {
+		
+		RatingDto rating = companyDao.getCompanyRating(companyId);
+		
+		return rating;
+	}
+
+}
