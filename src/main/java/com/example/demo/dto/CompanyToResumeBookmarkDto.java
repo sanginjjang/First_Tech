fc03ae4e143bed9fbe26a2e654db3a1285e13775
Han Sangin <hansam8456@naver.com>
2024-12-02 09:09:00 +0900

    dto main에 올립니다

---
+++ src/main/java/com/example/demo/dto/CompanyToResumeBookmarkDto.java
@@ -0,0 +1,8 @@
+package com.example.demo.dto;
+
+public class CompanyToResumeBookmarkDto {
+	private int bookmarkNum; // 1. 북마크 고유 번호
+	private String companyId; // 2. 기업 ID (FK)
+	private int resumeNum; // 3. 이력서 고유 번호 (FK)
+	private java.sql.Timestamp bookmarkDate; // 4. 북마크 등록일
+}
