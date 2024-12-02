7b33f5e9e6b3b75df971d098e35e3d2645d34cce
Han Sangin <hansam8456@naver.com>
2024-12-02 09:09:15 +0900

    Revert "dto main에 올립니다"
    This reverts commit fc03ae4e143bed9fbe26a2e654db3a1285e13775.

--- src/main/java/com/example/demo/dto/CompanyTechStackDto.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dto;
-
-public class CompanyTechStackDto {
-	private int companyTechStackNum; // 1. 기술 스택 고유 번호
-	private int applicationNum; // 2. 기업 공고 고유 번호 (FK)
-	private int techStackNum; // 3. 기술 스택 고유 번호 (FK)
-}
