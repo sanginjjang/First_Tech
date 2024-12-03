7d716151910aa45d5ec3636e5a7e2d73a0913010
An Hyepin <gpqls9524@naver.com>
2024-12-03 18:26:23 +0900

    1203 작업물

---
+++ src/main/java/com/example/demo/dao/IUserDao.java
@@ -0,0 +1,18 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.dto.UserDto;
+
+@Mapper
+public interface IUserDao {
+
+	public List<UserDto> getUserList();
+	public UserDto getUser(String userId);
+	public UserDto getUserByNickname(String userNickname);
+	public int insertUser(UserDto user);
+	
+	
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.IUserDao;
+import com.example.demo.dto.UserDto;
+
+@Service
+public class UserService implements IUserService {
+
+	@Autowired
+	IUserDao userDao;
+
+	@Override
+	public List<UserDto> getUserList() {
+		List<UserDto> users = userDao.getUserList();
+		return users;
+	}
+	
+	@Override
+	public int insertUser(UserDto user) {
+		int result = userDao.insertUser(user);
+		return result;
+	}
+	
+	@Override
+	public UserDto getUser(String userId) {
+		UserDto user = userDao.getUser(userId);
+		return user;
+	}
+
+	@Override
+	public UserDto getUserByNickname(String userNickname) {
+		UserDto user = userDao.getUserByNickname(userNickname);
+		return user;
+	}
+
+	
+}
