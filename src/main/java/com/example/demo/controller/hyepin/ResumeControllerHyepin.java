59c7b812b52fe66ffa3a889a196108390455d08c
An Hyepin <gpqls9524@naver.com>
2024-12-05 16:39:33 +0900

    1205 두번쨰 커밋

---
+++ src/main/java/com/example/demo/controller/hyepin/ResumeControllerHyepin.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller.hyepin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hyepin")
+public class ResumeControllerHyepin {
+	
+	@RequestMapping("/resumeViewDetails")
+	public void resumeViewDetails() {
+	}
+	
+	
+}
