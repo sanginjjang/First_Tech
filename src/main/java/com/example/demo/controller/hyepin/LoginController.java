1e2b991c3397738a17ccba6ee6a49f92e2c02499
An Hyepin <gpqls9524@naver.com>
2024-12-05 15:20:19 +0900

    1205 첫번쨰 커밋

--- src/main/java/com/example/demo/controller/hyepin/LoginController.java
+++ src/main/java/com/example/demo/controller/hyepin/LoginController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.dto.CompanyDto;
 import com.example.demo.dto.UserDto;
+import com.example.demo.service.ICompanyServiceHyepin;
 import com.example.demo.service.IUserService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,6 +23,9 @@ public class LoginController {
 	
 	@Autowired
 	IUserService userService;
+	
+	@Autowired
+	ICompanyServiceHyepin companyService;
 
 	@RequestMapping("/login")
 	public void login() {
@@ -43,7 +48,7 @@ public class LoginController {
 			result = "ready";
 		}else {
 			if(userService.idCheck(user)) {
-				result = "success";
+				result = "userSuccess";
 				UserDto u = userService.getUser(user.getUserId());
 				HttpSession session = request.getSession();
 				session.setAttribute("user", u);
@@ -60,6 +65,28 @@ public class LoginController {
 		session.invalidate();
 		return "/sangin/sum";
 	}
+	
+
+	@PostMapping("/companyLogin")
+	public @ResponseBody String companyLogin(CompanyDto companyDto, HttpServletRequest request) {
+		System.out.println(companyDto);
+		String result = "ready";
+		//ready, success, fail
+		if(companyDto.getCompanyId() == "" || companyDto.getCompanyPassword() == "") {
+			result = "ready";
+		}else {
+			if(companyService.idCheck(companyDto)) {
+				result = "comnpanySuccess";
+				CompanyDto company = companyService.getCompanyById(companyDto.getCompanyId());
+				HttpSession session = request.getSession();
+				session.setAttribute("company", company);
+			}else {
+				result = "fail";
+			}
+		}
+		return result;
+	}
+	
 	
 	
 	
